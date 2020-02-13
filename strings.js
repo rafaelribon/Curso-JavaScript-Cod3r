9b0e1e37e1b3666cdf5cc5f98f73c6e6e97d3d50
Rafael Ribon <rafael.ribon@consuting-for.edenred.com>
2020-02-13 18:49:05 -0300

    Fundamentos Parte 2

---
+++ strings.js
@@ -0,0 +1,18 @@
+const escola = "Cod3r"
+
+
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5)) // não existe JS não explode erro 
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf(3))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3)) 
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3,'e'))
+
+console.log('ANA,MARIA,PEDRO'.split(','))
+
+
