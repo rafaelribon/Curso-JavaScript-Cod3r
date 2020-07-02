f40f4b8aa519a942ce65aa76182091fe17c52159
Rafael Ribon <rafael.ribon@consuting-for.edenred.com>
2020-07-02 16:33:45 -0300

    Função

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,24 @@
+// Armazenado uma funcao em uma variavel 
+
+const imprimirSoma =  function (a, b) {
+    console.log(a + b)
+}
+
+
+imprimirSoma(2, 3)
+
+//Armazenando uma funçao arrow em uma variavel 
+
+const soma = (a ,b ) => {
+    return a + b 
+}
+
+console.log (soma(2, 3))
+
+
+//retorno implicito 
+const subtracao = (a, b) => a - b 
+console.log(subtracao(2, 3))
+
+const imprimir2 = a => console.log(a)
+console.log('Lega!!!!')
